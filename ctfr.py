c7db7a69f66987263ed09ed6c22458c35bf9ce17
vlad <daprostovseeto@gmail.com>
2021-04-23 16:34:13 +0300

    update project

--- ctfr.py
+++ ctfr.py
@@ -1,78 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-------------------------------------------------------------------------------
-	CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
-------------------------------------------------------------------------------
-"""
+import argparse
+import json
+from typing import NoReturn
 
-## # LIBRARIES # ##
-import re
 import requests
 
-## # CONTEXT VARIABLES # ##
-version = 1.2
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers',
+}
 
-## # MAIN FUNCTIONS # ##
 
 def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
-	parser.add_argument('-o', '--output', type=str, help="Output file.")
-	return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', type=str, required=True, help='Target domain.')
+    parser.add_argument('-o', '--output', type=str, help='Output file.', default='output.json')
+    return parser.parse_args()
 
-def banner():
-	global version
-	b = '''
-          ____ _____ _____ ____  
-         / ___|_   _|  ___|  _ \ 
-        | |     | | | |_  | |_) |
-        | |___  | | |  _| |  _ < 
-         \____| |_| |_|   |_| \_\\
-	
-     Version {v} - Hey don't miss AXFR!
-    Made by Sheila A. Berta (UnaPibaGeek)
-	'''.format(v=version)
-	print(b)
-	
-def clear_url(target):
-	return re.sub('.*www\.','',target,1).split('/')[0].strip()
 
-def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
+def clear_url(target: str) -> str:
+    return target.replace('www.', '', 1) if target.startswith('www.') else target
+
+
+def save_subdomains(subdomains: list, output_file: str) -> NoReturn:
+    with open(output_file, 'w') as f:
+        json.dump(subdomains, f, indent=2)
+
 
 def main():
-	banner()
-	args = parse_args()
-
-	subdomains = []
-	target = clear_url(args.domain)
-	output = args.output
-
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
-
-	if req.status_code != 200:
-		print("[X] Information not available!") 
-		exit(1)
-
-	for (key,value) in enumerate(req.json()):
-		subdomains.append(value['name_value'])
-
-	
-	print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
-
-	subdomains = sorted(set(subdomains))
-
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
-
-	print("\n\n[!]  Done. Have a nice day! ;).")
+    print('Start!')
+    args = parse_args()
+    target = clear_url(target=args.domain)
+    output = args.output
+    subdomains = []
+    resp = requests.get(f'https://crt.sh/?q=%.{target}&output=json', headers=headers, verify=True)
+    if resp.status_code == 200:
+        subdomains = sorted(set([item['name_value'] for item in resp.json()]))
+    save_subdomains(subdomains=subdomains, output_file=output)
+    print("Done. Have a nice day! ;).")
 
 
-main()
-	
+if __name__ == '__main__':
+    main()
